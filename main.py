97461af62c8e36353d824ebdb8f361b1c4c944a6
AlBiNoSs <107204374+AlBiNoSs@users.noreply.github.com>
2022-06-14 14:12:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pygame
 import os
 pygame.init()
+clock = pygame.time.Clock()
+
+img_hit = ["Hero_hit_R1","Hero_hit_R2","Hero_hit_R3","Hero_hit_R4"]
+img_left = ["Hero_stay_L1.png", "Hero_walk_L1.png", "Hero_walk_L2.png", "Hero_walk_L3.png", "Hero_walk_L4.png" ]
+img_right = ["Hero_stay_R2.png", "Hero_walk_R1.png", "Hero_walk_R2.png", "Hero_walk_R3.png", "Hero_walk_R4.png" ]
+img_up = ["Hero_stay_u1.png", "Hero_walk_u1.png", "Hero_walk_u2.png", "Hero_walk_u3.png", "Hero_walk_u4.png" ]
+img_down = ["Hero_walk0.png", "Hero_walk1.png", "Hero_walk2.png", "Hero_walk3.png", "Hero_walk4.png" ]
 
 stationary_d = pygame.image.load(os.path.join("Hero_walk0.png"))
 stationary_u = pygame.image.load(os.path.join("Hero_stay_u1.png"))
@@ -18,12 +25,85 @@ bg = pygame.transform.scale(bg_img,(1200,3000))
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, player_image, player_speed, player_x, player_y, dirrection):
         super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load(player_image), (65,65)) 
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-        self.dirrection = dirrection
+        self.stepIndex = 0
+        self.dirrection = pygame.math.Vector2()
 
+    def input(self):
+        keys = pygame.key.get_pressed()
+        global move_up
+        global move_down
+        global move_left
+        global move_right
+        if keys[pygame.K_UP]:
+            self.dirrection.y = -1
+            move_up = True
+            move_down = False
+        elif keys[pygame.K_DOWN]:
+            self.dirrection.y = 1
+            move_up = False
+            move_down = True
+        else:
+            self.dirrection.y = 0
+            move_up = False
+            move_down = False
+
+
+        if keys[pygame.K_RIGHT]:
+            self.dirrection.x = 1
+            move_right = True
+            move_left = False
+        elif keys[pygame.K_LEFT]:
+            self.dirrection.x = -1
+            move_left = True
+            move_right = False
+        else:
+            self.dirrection.x = 0
+            move_left = False
+            move_right = False
+    
+    def move(self, speed):
+        self.rect.center += self.dirrection * speed
+        print(self.rect.center)
+
+    def update(self):
+        self.input()
+        self.move(self.speed)
+        print(self.dirrection)
+
+    def reset(self):
+        global render_count
+        if render_count == 5: render_count == 0
+        if self.stepIndex >= 3:
+            self.stepIndex = 0
+        if move_left:
+            if render_count % 5 == 0:
+                window.blit(pygame.image.load(img_left[self.stepIndex]), (self.rect.x, self.rect.y))
+                self.stepIndex +=1
+        elif move_right:
+            window.blit(pygame.image.load(img_right[self.stepIndex]), (self.rect.x, self.rect.y))
+            self.stepIndex +=1
+        elif move_down:
+            window.blit(pygame.image.load(img_down[self.stepIndex]), (self.rect.x, self.rect.y))
+            self.stepIndex +=1
+        elif move_up:
+            window.blit(pygame.image.load(img_up[self.stepIndex]), (self.rect.x, self.rect.y))
+            self.stepIndex +=1
+        else:
+            if move[0]:
+                 window.blit(stationary_u, (self.rect.x, self.rect.y))
+            if move[1]:
+                window.blit(stationary_d, (self.rect.x, self.rect.y))
+            if move[2]:
+                window.blit(stationary_d, (self.rect.x, self.rect.y))
+            if move[3]:
+                window.blit(stationary_d, (self.rect.x, self.rect.y))
+            
+    
 class Player():
     def __init__(self, x, y):
         self.images_down = []
@@ -50,6 +130,33 @@ class Player():
         self.rect_x = x
         self.rect_y = y
 
+    def reset(self):
+        if self.stepIndex >= 3:
+            self.stepIndex = 0
+        if move_left:
+            window.blit(img_left[self.stepIndex], (self.rect_x, self.rect_y))
+            self.stepIndex +=1
+        elif move_right:
+            window.blit(img_right[self.stepIndex], (self.rect_x, self.rect_y))
+            self.stepIndex +=1
+        elif move_right:
+            window.blit(img_down[self.stepIndex], (self.rect_x, self.rect_y))
+            self.stepIndex +=1
+        elif move_right:
+            window.blit(img_up[self.stepIndex], (self.rect_x, self.rect_y))
+            self.stepIndex +=1
+        else:
+            if move[0]:
+                 window.blit(stationary_u, (x,y))
+            if move[1]:
+                window.blit(stationary_d, (x,y))
+            if move[2]:
+                window.blit(stationary_d, (x,y))
+            if move[3]:
+                window.blit(stationary_d, (x,y))
+            
+player = GameSprite("Hero_walk0.png", 4, 0, 270, 0)
+
 game = True
 width = 1200
 height = 600
@@ -63,8 +170,10 @@ move_right = False
 move_down = False
 move_up = False
 
-while game:
+render_count = 0
 
+while game:
+    
     window.blit(bg, (i, -3000+600))
     window.blit(bg, (i+width,0))
     window.blit(bg, (i - width, 0))
@@ -78,9 +187,13 @@ while game:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             game = False
+    
 
+    player.reset()
+    player.update()
     pygame.display.update()
-    pygame.time.delay(50)
+    clock.tick(15)
+
 
 
        
